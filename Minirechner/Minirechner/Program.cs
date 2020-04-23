5a1a9b4a6a99c4b54705ff149ce8d0e6fefe2ca7
02ratpas <02ratpas@rgtfo-me.it>
2020-04-23 15:26:24 +0200

    Addieren

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahl_1 = 0;
+            int Zahl_2 = 0;
+            string Rechenart = "";
+            int Ergebnis = 0;
+
+            Console.WriteLine("+ = Addieren, - = Subtrahieren, * = Multiplizieren, / = Dividierne");
+            Rechenart = Console.ReadLine();
+            Console.WriteLine("Zahl 1:");
+            Zahl_1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Zahl 2:");
+            Zahl_2 = Convert.ToInt16(Console.ReadLine());
+
+            if (Rechenart == "+")
+                Ergebnis = Addieren(Zahl_1, Zahl_2);
+            
+
+            Console.WriteLine("Ergebnis = " + Ergebnis);
+            Console.ReadKey();
+        }
+        static int Addieren(int Zahl_1, int Zahl_2)
+        {
+            int Erg = Zahl_1 + Zahl_2;
+            return Erg;
+        }
+    }
+}
